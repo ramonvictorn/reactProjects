3bf9eb9f73d00b58643479e2a4ece67a520d7433
Ramon Victor <ramonvictorn@gmail.com>
2019-01-24 18:29:54 -0200

    click ok

--- first-app/src/Button.jsx
+++ first-app/src/Button.jsx
@@ -4,20 +4,25 @@ import React from 'react';
 class Button extends React.Component {
    constructor(props) {
       super(props);
-      this.state = { x: 0, y: 0 };
+      this.state = { 
+         click: 0, 
+         name: this.props.txt, 
+      };
       this.props = 'n deu';
       this.handleClick = this.handleClick.bind(this);
    }
 
    handleClick(e) {
       //e.preventDefault();
-      console.log('Click happened');
+      this.setState({name:'Foi clicado'})
+      this.setState({click:this.state.click+1})
+      console.log('Click happened', this.state);
    }
 
    render() {
       return (
          <div>
-         <button onClick={() => this.handleClick()} className="text">{this.props.txt}</button>
+         <button onClick={() => this.handleClick()} className="text">{this.state.name}-{this.state.click}</button>
          </div>
       );
    }
